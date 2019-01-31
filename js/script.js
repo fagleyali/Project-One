82e78924808461ce8dcd09f0c194b915e299a2a2
Student Workspace <student@Candices-MacBook-Air.local>
2019-01-31 14:40:39 -0600

    insert pseodocode

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//1. Create 6 Squares of different colors for guessing. 
+//2. Write  RGB random number generator function.
+//3. Generate one RGB number to show at the top. 
+//4. Change the background of 5 squares according to the random numbers generated. 
+//5. Change the one square according to the RGB given at the top. 
+//6. Position of the correct colored square will also be changed in every refresh. 
